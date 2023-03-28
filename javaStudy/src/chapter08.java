3c6c2eabf6868565125386e27a9e604638e43686
baeseongho <bae9808@gmail.com>
2023-03-28 17:38:05 +0900

    First Commit 20230328

---
+++ javaStudy/src/chapter08.java
@@ -0,0 +1,26 @@
+
+class Car {
+
+    int num;
+    double gas;
+
+}
+
+
+
+public class chapter08 {
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car();
+
+        car1.num = 4567;
+        car1.gas = 11.1;
+
+        System.out.println("Name :"+car1.num);
+        System.out.println("Gas :"+car1.gas);
+
+    }
+
+
+}
